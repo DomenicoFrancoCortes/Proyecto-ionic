f489be925bc7725f9996e426875937b9a05b0202
Domenico Franco <domenicofranco.df@gmail.com>
2022-09-30 21:25:06 -0300

    primer commit

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  token = 'domenico';
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  login(){
+    localStorage.setItem('token', this.token)
+    this.router.navigate(["/home"]);
+    
+
+    
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+firebaseConfig: {
+  apiKey: "AIzaSyDfyC52FXGCmOuFnjyTxpoydA0PQofgNBQ",
+  authDomain: "curso-ionic-main.firebaseapp.com",
+  projectId: "curso-ionic-main",
+  storageBucket: "curso-ionic-main.appspot.com",
+  messagingSenderId: "53514908007",
+  appId: "1:53514908007:web:735d649afc1941b3c83fc2",
+  measurementId: "G-8ZKBL8CBP7"
+},
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
